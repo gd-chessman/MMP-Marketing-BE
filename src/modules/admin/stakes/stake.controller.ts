39485cbd973e822d87bf30a4683fb1f4c8552283
Le Van Quy <vannn.quy@gmail.com>
2025-06-27 14:07:15 +0700

    up

--- src/modules/admin/stakes/stake.controller.ts
+++ src/modules/admin/stakes/stake.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Query, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Param, ParseIntPipe, UseGuards, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { StakeService } from './stake.service';
 import { SearchStakesDto } from './dto/search-stakes.dto';
 import { JwtAdminGuard } from '../auth/jwt-admin.guard';
 
 @Controller('admin/stakes')
 @UseGuards(JwtAdminGuard)
+@UseInterceptors(ClassSerializerInterceptor)
 export class StakeController {
   constructor(private readonly stakeService: StakeService) {}
 
